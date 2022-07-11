c85156620cd4e15b4339d5100024d3c99035db03
janghan <bjh4135@gmail.com>
2022-07-11 12:07:11 +0900

    [#5] docs: only swagger

--- server/index.js
+++ server/index.js
@@ -1,12 +1,8 @@
 import 'dotenv/config'
 import { app } from './src/app.js';
-import { db } from './src/db/config/db.config.js'
 
 const PORT = process.env.PORT || 3000;
-db.connect(error => {
-    if (error) throw error
-    console.log("DB 연결 성공"); 
-})
+
 app.listen(PORT, () => {
     console.log(`서버 정상 실행 http://localhost:${PORT}`)
 })
--- server/src/app.js
+++ server/src/app.js
@@ -13,17 +13,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.send("express!"));
-
-// app.use("user", userRouter);
-// app.use("study", studyRouter);
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const swaggerSpec = YAML.load(path.join(__dirname, '../build/swagger.yaml'))
 
 app.use(
-  "/api-docs",
+  "/",
   swaggerUi.serve,
   swaggerUi.setup(swaggerSpec, { explorer: true })
 );
